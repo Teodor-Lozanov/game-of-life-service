d80e027ea319cfb69876f75e4c906e6345ce1186
Teodor Lozanov <teodorlozanov@Teodors-MacBook-Pro.local>
2026-06-19 15:10:54 +0300

    Improve cors config

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['http://localhost:4200'],
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
   });
   await app.listen(process.env.PORT ?? 3000);
 }
